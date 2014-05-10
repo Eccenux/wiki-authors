57d5766bd3d59f717a7dbc2d9e0f530379dc5c52
Eccenux <eccenux+git@gmail.com>
2014-05-10 11:36:36 +0200

    info/readme
    doxygen format

---
+++ .info.php
@@ -0,0 +1,38 @@
+<?php
+/*!
+	@mainpage Project description
+	
+	This project is aimed at getting very basic author statistics for a specified page oldid.
+	
+	@author
+		Copyright ©2010-2011 Maciej Jaros (pl:User:Nux, en:User:Nux)
+	@version
+		1.0.0
+	
+	@section License
+	
+	You can use this program under the terms of any of the below licenses.
+	Note that in any case you have to state the author.
+	
+	@note Logo used in the header was made by Artur Jan Fijałkowski (WarX)
+	@note http://pl.wikipedia.org/wiki/Plik:Wikimedia_Community_Logo-Toolserver.svg
+	
+	@subsection GPL GNU GPL v2
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License as
+	published by the Free Software Foundation; version 2 of the License.
+
+	This program is distributed in the hope that it will be useful, but
+	WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+	General Public License for more details at:
+	http://www.gnu.org/copyleft/gpl.html
+
+	@subsection CCBYSA CC-BY-SA 3.0
+	Creative Commons Attribution-ShareAlike 3.0 is described at:
+	http://creativecommons.org/licenses/by-sa/3.0/
+	
+	Polska wersja licencji:
+	http://creativecommons.org/licenses/by-sa/3.0/deed.pl
+*/
+?>

4d65ba7989c69603e037bc1b62526cd02bd2e913
Nux <egil@wp.pl>
2021-10-18 01:43:38 +0200

    en and fixes

--- lang/en.php
+++ lang/en.php
@@ -5,6 +5,10 @@ $arrLang = array(
 	//...
 	'Authors:stats note' => 'Note! The edits that were marked as minor were not taken into account.',
 	'Authors:title' => 'Page authors',
-	'Authors:oldid needed error' => 'The "oldid" parameter is needed.',
+	'Authors:oldid needed error' => 'The "oldid" parameter is required.
+		<p>This tools shows a list of authors based on oldid. Oldid is a revision id. You can get a revision id from a link to a specific version of any plwiki article.
+		Example: <a href="?oldid=6765334">authors of Astronomia article (as seen on the Ferbruary 28, 2007)</a>
+		(<a href="https://pl.wikipedia.org/wiki/?oldid=6765334" target="_blank" rel="noopener">Astronomia article on Polish Wikipedia (same date)</a>)
+	',
 )
 ?>
